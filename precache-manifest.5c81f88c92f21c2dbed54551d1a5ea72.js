08831c8691d1bfbac8635e02701baaffeb4ccaf6
ciamac-da <ciamac.davoudi@gmail.com>
2021-03-06 19:25:06 +0100

    Updates

---
+++ precache-manifest.5c81f88c92f21c2dbed54551d1a5ea72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58a670b01f46adacfa0091c5a9d32441",
+    "url": "/react-redux-zalh-plus-minus/index.html"
+  },
+  {
+    "revision": "0bd121f0a7d0cdd1e0dd",
+    "url": "/react-redux-zalh-plus-minus/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a4cf3b66b0592e165413",
+    "url": "/react-redux-zalh-plus-minus/static/js/2.d98f739f.chunk.js"
+  },
+  {
+    "revision": "c8e07e19562a4690a354be24150f8167",
+    "url": "/react-redux-zalh-plus-minus/static/js/2.d98f739f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0bd121f0a7d0cdd1e0dd",
+    "url": "/react-redux-zalh-plus-minus/static/js/main.95a215c3.chunk.js"
+  },
+  {
+    "revision": "8cdf3d2e9c17d51b10fe",
+    "url": "/react-redux-zalh-plus-minus/static/js/runtime-main.78f9d04e.js"
+  },
+  {
+    "revision": "91a8270bc889d0d9da041cfac870c193",
+    "url": "/react-redux-zalh-plus-minus/static/media/Mathe.91a8270b.jpg"
+  }
+]);
